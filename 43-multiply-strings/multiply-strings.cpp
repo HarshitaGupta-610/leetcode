60e4e6f99711a3abcc536a2692a7e3dfccf910b8
Harshita Gupta <harshitagupta.ambala@gmail.com>
2025-12-06 21:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (54.21%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string multiply(string a, string b) {
+
+        if (a == "0" || b == "0") return "0";
+
+        int n = a.size(), m = b.size();
+        vector<int> res(n + m, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (a[i] - '0') * (b[j] - '0');
+                int sum = mul + res[i + j + 1];
+
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        string ans = "";
+        int i = 0;
+        while (i < res.size() && res[i] == 0) i++;
+
+        for (; i < res.size(); i++) ans.push_back(res[i] + '0');
+
+        return ans;
+    }
+};
