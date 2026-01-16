44047aabea344ac1ea2f670c872b80fdf73d44a1
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-01-16 09:06:54 +0530

    Time: 0 ms (100%), Space: 27.2 MB (79.71%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int f = 0;
+        int l = s.size() - 1;
+       while( f <= l){
+        char temp = s[f];
+        s[f] = s[l];
+        s[l] = temp;
+        f++;
+        l--;
+        }
+        
+    }
+};
