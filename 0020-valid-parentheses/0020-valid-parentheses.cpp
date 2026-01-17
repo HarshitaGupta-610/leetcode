a9e4ef206843cf9656a82431490dd261218c1763
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-01-17 13:32:08 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s){
+            if(ch == '('){
+                st.push(')');
+            }
+            else if(ch == '['){
+                st.push(']');
+            }
+           else if(ch == '{'){
+                st.push('}');
+            }
+            else{
+                if( st.empty() ||st.top() != ch){
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        if(st.empty() == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
