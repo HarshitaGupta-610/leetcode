9e6f5c42901dcfc7d45a3d32083332fc9b290268
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-02-17 10:43:37 +0530

    Time: 0 ms (100%), Space: 8.1 MB (46.31%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+
+        int low = 0, high = 0;
+
+        for(char c : s){
+
+            if(c == '('){
+                low++;
+                high++;
+            }
+
+            else if(c == ')'){
+                low--;
+                high--;
+            }
+
+            else{ 
+                low--;    
+                high++;    
+            }
+
+            if(high < 0)  {
+                return false;
+            }
+
+            if(low < 0)  {  
+                low = 0;
+            }
+        }
+
+       if(low == 0){
+        return true;
+       }
+       return false;
+    }
+};
