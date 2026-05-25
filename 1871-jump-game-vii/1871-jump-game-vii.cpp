0456dc8c9f5d8f8369dd038c29f47b98e37840c4
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-05-25 23:00:49 +0530

    Time: 20 ms (15.06%), Space: 24.7 MB (42.86%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+
+        int n = s.size();
+
+        queue<int> q;
+        q.push(0);
+
+        vector<bool> visited(n, false);
+        visited[0] = true;
+
+        int farthest = 1;
+
+        while (!q.empty()) {
+
+            int i = q.front();
+            q.pop();
+
+            int start = max(i + minJump, farthest);
+            int end = min(i + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+
+                if (s[j] == '0' && !visited[j]) {
+
+                    if (j == n - 1){
+                        return true;
+                    }
+
+                    visited[j] = true;
+                    q.push(j);
+                }
+            }
+
+            farthest = end + 1;
+        }
+
+        return n == 1;
+    }
+};
