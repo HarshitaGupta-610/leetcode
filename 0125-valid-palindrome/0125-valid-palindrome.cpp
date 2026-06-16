8b120b37578f2fe0973f85aa4215ff2fbcc6dba8
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-06-16 17:44:17 +0530

    Time: 1 ms (43.66%), Space: 9.8 MB (94.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i = 0;
+        int j = n - 1;
+        while(i <= j){
+            if(s[i] == ' '){
+                i++;
+                continue;
+            }
+            if( s[j] == ' '){
+                j--;
+                continue;
+            }
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j])){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
