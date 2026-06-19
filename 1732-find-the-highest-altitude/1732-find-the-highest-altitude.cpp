00a7f8145f8755ee983ebc6f031e13ec268a768e
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-06-20 03:16:02 +0530

    Time: 0 ms (100%), Space: 10.6 MB (99.89%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int sum = 0;
+        int maxi = 0;
+        for(int g : gain){
+           sum+=g;
+           maxi = max(maxi , sum);
+        }
+        return maxi;
+
+    }
+};
