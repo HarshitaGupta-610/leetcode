c0107de8b4ab9b4bd3beb49dffffeb7ab4ce9f57
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-06-22 15:56:39 +0530

    Time: 0 ms (100%), Space: 20.4 MB (71.51%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int count = 0;
+        int reach = 0;
+        int last = 0;
+        for(int i = 0 ; i < nums.size() - 1 ; i++){
+            reach = max(reach , i + nums[i]);
+            if(i == last){
+                last = reach;
+                count++;
+            }
+        }
+        return count;
+    }
+};
