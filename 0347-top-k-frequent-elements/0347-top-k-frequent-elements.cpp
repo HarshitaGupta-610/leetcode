e81b0cf303b7ec88336fcf33476a5f70a2ff43cd
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-16 23:23:03 +0530

    Time: 1 ms (65.11%), Space: 17.8 MB (97.48%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,37 +4,58 @@ public:
        int n = nums.size();
      
 
-     //Brute force: O(N X N)
-unordered_map<int,int>m;
-for(int i = 0 ; i < n ;i++){
-    int count = 0;
-    for(int j = 0 ; j < n ; j++ ){
-        if(nums[i] == nums[j]){
-            count++;
-        }
+// //Brute force: O(N X N) :pehle map mein sabka count daala , fir freq ko priority consider krte huye pairs banke sort kre (pair mein pehla element freq consider kara) uske bad ans vector mein elements yani second value pairs ki dalte gaye aur element set mein daalte gaye aur jaise hi answer vector ka size k hua toh answer mil gaya
+
+// unordered_map<int,int>m;
+// for(int i = 0 ; i < n ;i++){
+//     int count = 0;
+//     for(int j = 0 ; j < n ; j++ ){
+//         if(nums[i] == nums[j]){
+//             count++;
+//         }
+//     }
+//     m[nums[i]] = count;
+// }
+// vector<pair<int,int>> freq;
+// for(auto it : m){
+// freq.push_back({it.second , it.first});
+// }
+// sort(freq.begin() , freq.end() , greater<pair<int,int>>());
+// vector<int>ans;
+// unordered_set<int>s;
+// for(auto it : freq){
+//     if(s.find(it.second) == s.end()){
+//         ans.push_back(it.second);
+//         s.insert(it.second);
+//     }
+//      if(ans.size() ==k){
+//         return ans;
+//     }
+// } 
+// return{};
+//     }
+
+
+
+//optimal approach : O(n logn) : pehle short krege aur ek hi traversal mein freq increase krte rhege
+sort(nums.begin() , nums.end());
+vector<pair<int,int>>freq;
+int count = 1;
+for(int i = 0 ; i < n; i++){
+    while( i + 1 < n && nums[i] == nums[i+1]){
+        count++;
+        i++;
     }
-    m[nums[i]] = count;
-}
+    freq.push_back({count , nums[i]});
+    count = 1;
 
-
-vector<pair<int,int>> freq;
-for(auto it : m){
-freq.push_back({it.second , it.first});
 }
 sort(freq.begin() , freq.end() , greater<pair<int,int>>());
 vector<int>ans;
-unordered_set<int>s;
-for(auto it : freq){
-    if(s.find(it.second) == s.end()){
-        ans.push_back(it.second);
-        s.insert(it.second);
-    }
-     if(ans.size() ==k){
-        return ans;
-    }
+for(int i = 0 ; i < k ; i++){
+    ans.push_back(freq[i].second);
 }
-   
-    return{};
+return ans;
     }
 
 };
