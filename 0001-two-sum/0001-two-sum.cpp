563c96b491902a6955dbc215fa6f5e292d9651f9
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-16 18:59:22 +0530

    Time: 34 ms (35.2%), Space: 14.1 MB (69.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>mapo;
-
-        for(int i = 0 ; i < nums.size();i++){
-            int complement = target - nums[i];
-
-            if(mapo.find(complement) != mapo.end()){
-                return {mapo[complement] , i};
+        int n = nums.size();
+      //Brute force : O(NXN) , use loops to traverse and find if any pair exists
+      for(int i = 0 ; i < n ; i++){
+        for(int j = i+1 ; j < n ; j++){
+            if(nums[i] + nums[j] == target){
+                return {i,j};
             }
-
-            mapo[nums[i]]=i;
         }
-        return {};
+      }
+      return {};
     }
 };
