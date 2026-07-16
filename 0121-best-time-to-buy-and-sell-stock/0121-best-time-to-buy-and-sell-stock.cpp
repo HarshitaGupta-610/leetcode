647deb3a23f0d2b5d93be0f35e121922e693a3b3
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-16 19:15:28 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.92%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        int minprice = prices[0];
+       int maxi = 0;
+       int n = prices.size();
+    //    //brute force  :O(n²) Because for each element, we are checking every future element nested loops.
+    //    for(int i = 0 ; i < n ; i++){
+    //     for(int j = i +1 ; j < n ; j++){
+    //         maxi = max(maxi , prices[j] - prices[i]);
+    //     }
+    //    }
+    //    return maxi;
 
-        for(int price : prices){
-            minprice = min(price,minprice);
-            ans = max (ans,price - minprice);
-        }
-        return ans;
+
+    //traverse krte krte hi profit calcualte krte jao max profit tabhi hoga jab min mein kahreeda ho O(n)
+    int kamPrice = INT_MAX;
+    for(int i = 0 ; i < n ; i++){
+if(prices[i] < kamPrice){
+    kamPrice = prices[i];
+}
+maxi = max(maxi , prices[i] - kamPrice);
+    }
+    return maxi;
     }
 };
