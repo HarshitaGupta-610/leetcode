d581165b455e28e82c04c8ed30ca1da37088261f
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-16 19:19:28 +0530

    Time: 34 ms (78.96%), Space: 82.8 MB (78.53%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+
+
+
+        // //Brute force :  O(n²) Because for each element, we are checking every future element nested loops.
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = i + 1 ; j < n ; j++){
+        //         if(nums[i] == nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+
+
+        //Better approach :O(nlogn) sort and keep coparing with previous
+        sort(nums.begin() , nums.end());
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
