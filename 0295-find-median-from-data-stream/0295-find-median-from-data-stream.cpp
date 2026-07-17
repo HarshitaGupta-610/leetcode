78f2f23ca35f122ba55e5aaaeb54b9f74c908927
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-18 03:35:41 +0530

    Time: 47 ms (97.46%), Space: 148.5 MB (64.38%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -24,20 +24,57 @@ public:
 
 //Better Approach : O(log n)iterator ki insertion + O(n) for elements insertion ~ O(n)
 //element ko daalo hi sahi position pe
-vector<int>v;
+// vector<int>v;
+// void addNum(int num){
+//     auto it = lower_bound(v.begin() , v.end() , num); // us number ki sahi posiiton pe insertion ho jaaye
+//     v.insert(it,num);
+// }
+// double findMedian(){
+//     int n = v.size();
+//     if( n % 2 != 0){
+//         return v[n/2];
+//     }else{
+//         return (v[n/2] + v[n/2 - 1]) / 2.0;  
+//     }
+// }
+
+//Optimal : 
+/*median ke liye left side ka maximum element aur right side ka minimum element chahiye
+maximum top pe ho toh max heap , min top pe ho toh min heap
+left ke liye max heap banayege aur right ke liye min heap
+*/
+priority_queue<int>left;
+priority_queue<int,vector<int>,greater<int>>right;
 void addNum(int num){
-    auto it = lower_bound(v.begin() , v.end() , num); // us number ki sahi posiiton pe insertion ho jaaye
-    v.insert(it,num);
-}
-double findMedian(){
-    int n = v.size();
-    if( n % 2 != 0){
-        return v[n/2];
-    }else{
-        return (v[n/2] + v[n/2 - 1]) / 2.0;  
+    if(left.empty() || num <= left.top()){
+        left.push(num);
+    }
+    else{
+        right.push(num);
+    }
+    if(left.size() > right.size() + 1){
+        right.push(left.top());
+        left.pop();
+    }
+    else if(right.size() > left.size()){
+left.push(right.top());
+right.pop();
     }
 }
+double findMedian(){
+    if(left.size() == right.size()){
+        return (left.top() + right.top()) / 2.0;
+    }
+    else{
+        return left.top();
+    }
+}
+
+
 };
+
+
+
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
