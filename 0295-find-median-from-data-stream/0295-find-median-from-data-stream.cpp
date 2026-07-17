a9efeb66a764c962ee59dd8d3e8b5e11252d09dd
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-18 03:16:34 +0530

    Time: 527 ms (5.01%), Space: 148.6 MB (64.38%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,67 +1,42 @@
 class MedianFinder {
 public:
-
-    // Left half -> Max Heap
-    // Isme hamesha chhota half store hoga.
-    // Top pe left half ka sabse bada element hoga.
-    priority_queue<int> left;
-
-    // Right half -> Min Heap
-    // Isme hamesha bada half store hoga.
-    // Top pe right half ka sabse chhota element hoga.
-    priority_queue<int, vector<int>, greater<int>> right;
-
     MedianFinder() {
-
+        
     }
 
-    void addNum(int num) {
+    // Brute : O(nlogn ) as har time apne element daalke sort krte rahege
+    // vector<int>v;
+    // void addNum(int num) {
+    //     v.push_back(num);
+    // }
+    
+    // double findMedian() {
+    //     sort(v.begin() , v.end());
+    //     int n = v.size();
+    //     if( n % 2 == 0){
+    //         return (v[n/2] + v[(n/2) - 1] ) /  2.0;
+    //     }
+    //     else{
+    //         return v[n/2];
+    //     }
+    // }
 
-        // Agar left heap empty hai ya num left ke top se chhota/equal hai
-        // to num left half me jayega.
-        if (left.empty() || num <= left.top()) {
-            left.push(num);
-        }
-        else {
-            // warna right half me jayega.
-            right.push(num);
-        }
 
-        // ---------------- BALANCING ----------------
-
-        // Agar left heap me 2 se zyada extra elements ho gaye
-        // to uska top right heap me bhej do.
-        if (left.size() > right.size() + 1) {
-            right.push(left.top());
-            left.pop();
-        }
-
-        // Agar right heap me zyada elements ho gaye
-        // to uska smallest element left heap me bhej do.
-        else if (right.size() > left.size() + 1) {
-            left.push(right.top());
-            right.pop();
-        }
+//Better Approach : O(log n)iterator ki insertion + O(n) for elements insertion ~ O(n)
+//element ko daalo hi sahi position pe
+vector<int>v;
+void addNum(int num){
+    auto it = lower_bound(v.begin() , v.end() , num); // us number ki sahi posiiton pe insertion ho jaaye
+    v.insert(it,num);
+}
+double findMedian(){
+    int n = v.size();
+    if( n % 2 != 0){
+        return v[n/2];
+    }else{
+        return (v[n/2] + v[n/2 - 1]) / 2.0;  
     }
-
-    double findMedian() {
-
-        // Dono heaps ka size equal hai
-        // Median = dono tops ka average
-        if (left.size() == right.size()) {
-
-            // 2.0 likhna zaruri hai warna integer division ho jayegi.
-            return (left.top() + right.top()) / 2.0;
-        }
-
-        // Agar left heap bada hai
-        if (left.size() > right.size()) {
-            return left.top();
-        }
-
-        // Agar right heap bada hai
-        return right.top();
-    }
+}
 };
 
 /**
