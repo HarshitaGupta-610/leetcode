13ceed21735157c900a6b4283287fbe0a67c1c5f
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-20 20:32:46 +0530

    Time: 4 ms (85.8%), Space: 44.9 MB (48.5%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,22 +1,62 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+int greed = g.size();
+int sizes = s.size();
+        // //brute froce : O(N X M + nlogn + mlogm)
+        // //cookies aur greed ko sort karke min needs dheere
+        // //fulfill karte jao
+        // sort(g.begin(),g.end());
+        // sort(s.begin(),s.end());
+        // vector<int>used(sizes,0);
+        // int count = 0;
+        // for(int i = 0 ; i < greed ; i++){
+        //     for(int j = 0 ; j < sizes ; j++){
+        //         if(s[j] >= g[i] && used[j] ==0 ){
+        //             used[j] = 1;
+        //             count++;
+        //             break; // is bache ko milgaya agli greed dekho
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+        //Brute force : O(nlong + mlogm)
+        //ek bar j use hogya toh next j se start karo
+        // sort(g.begin() , g.end());
+        // sort(s.begin() , s.end());
+        // int start = 0 ;//cookies index to start with 
+        // int ans = 0;
+        // for(int i = 0 ; i < greed ; i++){
+        //     for(int j = start ; j < sizes ; j++){
+        //         if(s[j] >= g[i]){
+        //             ans++;
+        //             start = j +1;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+
+        //optimal : O(nlogn + mlogm)
+        /*sort karke simulataneously tarverse karte raho aur cookies assign karte raho
+        */
         sort(g.begin() , g.end());
         sort(s.begin() , s.end());
-        int n1 = g.size();
-        int n2 = s.size();
-        int i = 0  , j = 0 , count = 0;
-        while( i < n1 && j < n2){
-            if(s[j] >= g[i]){
+        int i = 0 , j = 0;// i is for student , j for cookies
 
-                i++;
-                j++;
-                count++;
-            }
-            else{
-                j++;
-            }
+        while( i < greed && j < sizes){
+if(s[j] >= g[i]){
+    i++ , j++;
+
+}
+else{
+    j++;//ye cookie toh kisi kaam ka nahi aage wale ka dekhege
+}
         }
-        return count;
+        return i;
     }
 };
