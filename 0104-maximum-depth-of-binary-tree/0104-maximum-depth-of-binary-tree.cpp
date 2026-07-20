4fe632a4ab3717ad3e80809da5e32a62bf90ff25
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-21 00:47:37 +0530

    Time: 0 ms (100%), Space: 22.2 MB (19.91%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,12 +12,68 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if( root == NULL){
+if(root == NULL) {
             return 0;
         }
-        int leftHeight = maxDepth(root -> left);
-        int rightHeight = maxDepth(root -> right);
-        int ans =  1+ max(leftHeight , rightHeight);
-        return ans;
+
+
+        //1.Approach;
+        // BFS (Level Order Traversal) , number of levels = depth
+        queue<TreeNode*> q;
+        // Traversal root node se start hoga.
+        q.push(root);
+ // Ye variable final maximum depth store karega.
+        int depth = 0;
+        // Jab tak queue empty nahi hoti,
+        // tab tak tree me nodes bache hue hain.
+        while(!q.empty()) {
+            // Queue me iss time jitne nodes hain,
+            // woh saare ek hi level ke belong karte hain.
+            //
+            // Example:
+            //
+            //        1
+            //       / \
+            //      2   3
+            //
+            // Queue = [2,3]
+            // size = 2
+            //
+            // Matlab current level me 2 nodes hain.
+            int size = q.size();
+
+            // Ab current level ke saare nodes process karenge.
+            while(size--) {
+
+                // Queue ka front node uthao.
+                TreeNode* node = q.front();
+
+                // Us node ko queue se hata do.
+                q.pop();
+
+                // Agar left child exist karta hai,
+                // to use queue me daal do.
+                //
+                // Ye current level ka part nahi hai,
+                // ye NEXT level ka node hai.
+                if(node->left != NULL) {
+                    q.push(node->left);
+                }
+
+                // Agar right child exist karta hai,
+                // to use bhi queue me daal do.
+                if(node->right != NULL) {
+                    q.push(node->right);
+                }
+            }
+            // Jab inner while loop khatam ho gaya,
+            // iska matlab current level ke SAARE nodes visit ho gaye.
+            // Isliye ek complete level finish hua.
+            depth++;
+        }
+
+        // Queue empty ho gayi.
+        // Matlab saare levels traverse ho chuke hain.
+        return depth;
     }
 };
