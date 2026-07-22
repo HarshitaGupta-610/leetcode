35b5b1f35688a4d738eb6f563b52da5303e6b366
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-22 19:30:11 +0530

    Time: 1 ms (39.38%), Space: 17 MB (73.31%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,36 +11,28 @@
  */
 class Solution {
 public:
+//Time Complexity: O(n) kyunki har node ko sirf ek baar visit (pop/process) karte hain.
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root == NULL){
-            return ans;
-        }
-        queue<TreeNode*>q;
-        
-           q.push(root);
-           while(!q.empty()){
-
-            int n = q.size();
-            vector<int>level;
-            while(n--){
-
+vector<vector<int>>a;
+if(root ==NULL){
+    return a;
+}// koi tree nahi hai koi ans nahi
+    /*"I visit one node, then I put its children in the queue. Then I visit those children one by one, and each of them puts their own children into the queue."*/
+    //Queue mein root daalo, phir jab bhi koi node bahar nikalo (pop), uske left aur right children ko queue mein daal do; ye process queue khali hone tak repeat karo."
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int>level;
+        while(size--){//Current level mein total kitne nodes hain
             TreeNode* node = q.front();
             q.pop();
-          level.push_back(node->val);
-           if(node -> left != NULL){
-            q.push(node->left);
-           }
-           if(node -> right != NULL){
-            q.push(node->right);
-           }
-           }
-
-           ans.push_back(level);
-       
+            level.push_back(node->val);
+            if(node -> left != NULL) q.push(node->left);
+            if(node -> right != NULL) q.push(node->right);
+        }
+        a.push_back(level);//size 0 matlab level processed
     }
-    return ans;
+    return a;
     }
-
-    
 };
