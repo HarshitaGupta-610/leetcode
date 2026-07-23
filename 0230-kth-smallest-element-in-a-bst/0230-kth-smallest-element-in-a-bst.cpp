274207f09cc30a2fa66a766d2e8351eea2fea54b
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-23 13:53:13 +0530

    Time: 0 ms (100%), Space: 24.5 MB (27.9%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,30 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ * };
+ */
+
 class Solution {
 public:
-    int remaining; 
-    int answer;
 
-    void inorder(TreeNode* root) {
-        if (root == NULL || remaining == 0) {
+    int count = 0;
+    int ans = 0;
+
+    void dfs(TreeNode* root, int k){
+
+        if(root == NULL)
+            return;
+
+        // Left subtree
+        dfs(root->left, k);
+
+        // Current node visit
+        count++;
+
+        // Agar k-th node mil gaya
+        if(count == k){
+            ans = root->val;
             return;
         }
 
-        inorder(root->left);
-
-        remaining--;
-        if (remaining == 0) {     
-            answer = root->val;
-            return;
-        }
-
-        inorder(root->right);
+        // Right subtree
+        dfs(root->right, k);
     }
 
     int kthSmallest(TreeNode* root, int k) {
-        remaining = k;
-        inorder(root);
-        return answer;
+
+        dfs(root, k);
+
+        return ans;
     }
 };
