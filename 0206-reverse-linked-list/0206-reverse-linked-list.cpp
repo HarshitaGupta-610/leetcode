2b4ed33a8d8860e1ab10683f5458ad6b58a55477
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-24 02:07:25 +0530

    Time: 0 ms (100%), Space: 13.5 MB (42.21%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -14,20 +14,36 @@ public:
         if(head == NULL)return head; //empty list
 
 
-        //Brute : O(n)  vector mein value store krke baha nikalo end se
-        vector<int>v;
-        ListNode* temp = head;
-        while(temp != NULL){
-            v.push_back(temp->val);
-            temp = temp -> next;
+        // //Brute : O(n)  vector mein value store krke baha nikalo end se
+        // vector<int>v;
+        // ListNode* temp = head;
+        // while(temp != NULL){
+        //     v.push_back(temp->val);
+        //     temp = temp -> next;
+        // }
+        // temp = head;
+        // int i =v.size()-1;
+        // while(temp!= NULL){
+        //     temp->val = v[i];
+        //     i--;
+        //     temp = temp -> next;
+        // }
+        // return head;
+
+//Optimal : O(N) 3 pointers
+  ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr != NULL){
+            // Next node save karo
+            ListNode* next = curr->next;
+            // Pointer reverse karo
+            curr->next = prev;
+            // Prev aage badhao
+            prev = curr;
+            // Current aage badhao
+            curr = next;
         }
-        temp = head;
-        int i =v.size()-1;
-        while(temp!= NULL){
-            temp->val = v[i];
-            i--;
-            temp = temp -> next;
-        }
-        return head;
+        // Prev hi naya head hai
+        return prev;
     }
 };
