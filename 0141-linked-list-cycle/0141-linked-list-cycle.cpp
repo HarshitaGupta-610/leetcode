9a17c2309842e3b00bb709ce9cc6e076ca238d40
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-26 16:59:09 +0530

    Time: 14 ms (10.56%), Space: 11.9 MB (23.87%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        //Brute Force: O()store the addresses and if we get same address again tehre is cycle
-        unordered_set<ListNode*>v;//visited nodes
-        ListNode* temp = head;
-        while(temp  != NULL){//apan last node tak jayege
-            if(v.find(temp) != v.end()) return true;//address already there
-            v.insert(temp); 
-            temp = temp->next;
-        }
-        return false;
+        //Brute Force: O(N)store the addresses and if we get same address again tehre is cycle
+        // unordered_set<ListNode*>v;//visited nodes
+        // ListNode* temp = head;
+        // while(temp  != NULL){//apan last node tak jayege
+        //     if(v.find(temp) != v.end()) return true;//address already there
+        //     v.insert(temp); 
+        //     temp = temp->next;
+        // }
+        // return false;
+
+
+        //Optimal Approach : O(N)(Floyd's via 2 pointers)
+                // Dono pointers head se start karenge
+        ListNode* slow = head;
+        ListNode* fast = head;
+while(fast != NULL && fast -> next != NULL){//jab tak fast aur uska next exists
+    slow = slow -> next;// slow ek time pe ek step chalega
+    fast = fast -> next -> next;//fast ek time pe do step
+    if(slow == fast) return true;//agarr donon meet krjayein toh cycle
+}
+return false;
    }
 
 };
