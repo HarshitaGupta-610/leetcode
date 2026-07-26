0e74682bd85dd4f9e8211f151b453fdff00c73c7
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-27 01:40:29 +0530

    Time: 61 ms (33.57%), Space: 23.8 MB (86.81%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -20,16 +20,33 @@ public:
         //     a=a->next;
         // }
         // return NULL;
-        //Better : O(M+N): a ki sari store krlo , b ki find kro a mein hain ya nahi
-        unordered_set<ListNode*>s;
-        while(headA){
-            s.insert(headA);
-            headA = headA -> next;
-        }
-            while(headB){
-            if(s.find(headB) != s.end()) return headB;
-            headB = headB -> next;
-        }
-        return NULL;
+    //     //Better : O(M+N): a ki sari store krlo , b ki find kro a mein hain ya nahi
+    //     unordered_set<ListNode*>s;
+    //     while(headA){
+    //         s.insert(headA);
+    //         headA = headA -> next;
+    //     }
+    //         while(headB){
+    //         if(s.find(headB) != s.end()) return headB;
+    //         headB = headB -> next;
+    //     }
+    //     return NULL;
+    // }
+
+    //Optimal : O(M+N)
+    /*The difference between the list lengths is exactly the 
+    difference in their unique parts.When the pointers switch lists,
+     each pointer walks through the other list's unique part.
+So the pointer that was ahead becomes behind by the same amount,
+ and eventually both get aligned at the beginning of the common part.*/
+    ListNode* a= headA;
+    ListNode* b= headB;
+    while(a!=b){
+        if(a == NULL) a = headB;
+        else a = a-> next;
+        if(b==NULL) b = headA;
+        else b = b-> next;
+    }
+    return a;
     }
 };
