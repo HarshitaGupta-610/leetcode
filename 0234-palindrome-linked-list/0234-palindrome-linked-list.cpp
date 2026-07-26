c4872c845fd0318ce5cc20bd80a3e8a1245db0c6
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-27 02:25:14 +0530

    Time: 0 ms (100%), Space: 117.9 MB (82.31%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -11,21 +11,53 @@
 class Solution {
 public:
 //Brute Force: O(N) add in vectors the values and then compare pallindrome
- bool isPalindrome(ListNode* head) {
-vector<int>num;
-while(head!=NULL){
-    num.push_back(head->val);
-    head = head -> next;
-}
-int i = 0 , j = num.size()-1;
-while(i<j){
-    if(num[i] == num[j]) {
-        i++;
-        j--;
-    }else{
-        return false;
+//bool isPalindrome(ListNode* head) {
+// vector<int>num;
+// while(head!=NULL){
+//     num.push_back(head->val);
+//     head = head -> next;
+// }
+// int i = 0 , j = num.size()-1;
+// while(i<j){
+//     if(num[i] == num[j]) {
+//         i++;
+//         j--;
+//     }else{
+//         return false;
+//     }
+// }
+// return true;
+//     }
+
+
+//Optimal :Find the middle using slow-fast pointers, reverse the second half, and compare both halves node by node. (O(N) )
+ListNode* Rev(ListNode* head){
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+    while(curr){
+        ListNode* next = curr->next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next;
     }
+    return prev;
 }
+bool isPalindrome(ListNode* head) {
+if(head == NULL && head -> next == NULL) return true;
+ListNode* slow = head;
+ListNode* fast = head;
+while(fast-> next != NULL && fast -> next -> next != NULL){
+    slow = slow->next;
+    fast = fast->next->next;
+}
+ListNode* second = Rev(slow->next);
+ListNode* first = head;
+while(second!= NULL){
+    if(first->val != second -> val) return false;
+    first = first -> next;
+    second = second->next;
+}
+
 return true;
-    }
+}
 };
