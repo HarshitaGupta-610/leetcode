55789d0c4b3c3d1d98183fe09563ceacb8367cb9
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-27 01:01:36 +0530

    Time: 72 ms (17.9%), Space: 28.7 MB (15.74%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -10,14 +10,25 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         //Brute : O(N*M) for every node of a , check in b
-        ListNode* a = headA;
-        while(a != NULL){
-            ListNode* b = headB;
-            while(b!=NULL){
-                if(a==b) return a;
-                b=b->next;
-            }
-            a=a->next;
+        // ListNode* a = headA;
+        // while(a != NULL){
+        //     ListNode* b = headB;
+        //     while(b!=NULL){
+        //         if(a==b) return a;
+        //         b=b->next;
+        //     }
+        //     a=a->next;
+        // }
+        // return NULL;
+        //Better : O(M+N): a ki sari store krlo , b ki find kro a mein hain ya nahi
+        unordered_set<ListNode*>s;
+        while(headA){
+            s.insert(headA);
+            headA = headA -> next;
+        }
+            while(headB){
+            if(s.find(headB) != s.end()) return headB;
+            headB = headB -> next;
         }
         return NULL;
     }
