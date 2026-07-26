d3a2a1119fcf0e312e82c1e1d3a8e7400929e257
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-26 15:56:27 +0530

    Time: 0 ms (100%), Space: 19.6 MB (28.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -13,7 +13,7 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 
 
-        //Brute : O(N) traverse both and store in vector then sort
+        //Brute : O(N + M) traverse both and store in vector then sort
 //         vector<int>lists;
 //         while(list1 != NULL){
 // lists.push_back(list1 -> val);
@@ -33,7 +33,7 @@ public:
 // return dummy-> next;
 
 
-// //Better Approach :O(N)  Create a brand new linked list
+// //Better Approach :O(N + M)  Create a brand new linked list
 // ListNode* dummy = new ListNode(-1);
 // ListNode* temp =  dummy;
 // while(list1 && list2){
@@ -60,23 +60,38 @@ public:
 //     }
 
 
-//optimal : O(N): keep connecting the exisitng
-ListNode* temp = new ListNode(-1);
-ListNode* dummy = temp;
-while(list1 && list2){
-    if(list1 -> val <= list2-> val){
-        temp -> next = list1;
-        list1 = list1 -> next;
-    }else{
-        temp -> next = list2;
-        list2 = list2 -> next;
+//optimal : O(N + M): keep connecting the exisitng
+//temp denotes current position where we need to insert
+// ListNode* temp = new ListNode(-1);
+// ListNode* dummy = temp;
+// while(list1 && list2){
+//     if(list1 -> val <= list2-> val){
+//         temp -> next = list1;
+//         list1 = list1 -> next;
+//     }else{
+//         temp -> next = list2;
+//         list2 = list2 -> next;
 
-    }
-    temp = temp -> next;
+//     }
+//     temp = temp -> next;
 
+// }
+// if(list1)temp->next = list1;
+// else temp -> next = list2;
+// return dummy -> next;
+//     }
+
+//Recursive Approach : O(N+M)
+//ek list khatam toh answer dusri list hai
+if(list1 == NULL)return list2;
+if(list2 == NULL)return list1;
+//Jo node chota hai usmein hum baaki lists jodege
+if(list1 -> val <= list2 -> val){
+    list1 -> next = mergeTwoLists(list1->next , list2);
+    return list1;
+}else{
+    list2 -> next = mergeTwoLists(list1 , list2 -> next);
+    return list2;
 }
-if(list1)temp->next = list1;
-else temp -> next = list2;
-return dummy -> next;
     }
 };
