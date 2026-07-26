bd43141c6ba428e0e6791b32a846d60845a3e5eb
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-07-26 15:43:42 +0530

    Time: 0 ms (100%), Space: 19.5 MB (28.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -33,29 +33,50 @@ public:
 // return dummy-> next;
 
 
-//Better Approach :O(N)  Create a brand new linked list
-ListNode* dummy = new ListNode(-1);
-ListNode* temp =  dummy;
+// //Better Approach :O(N)  Create a brand new linked list
+// ListNode* dummy = new ListNode(-1);
+// ListNode* temp =  dummy;
+// while(list1 && list2){
+//     if(list1->val <= list2 -> val){
+//         temp -> next = new ListNode(list1->val);
+//         list1 = list1 -> next;
+//     }else{
+//         temp -> next = new ListNode(list2 -> val);
+//         list2 = list2 -> next;
+//     }
+//     temp = temp -> next;
+// }
+// while(list1){
+//     temp -> next = new ListNode(list1 -> val);
+//     list1 = list1 -> next;
+//     temp = temp -> next;
+// }
+// while(list2){
+//     temp -> next = new ListNode(list2 -> val);
+//     list2 = list2 -> next;
+//     temp = temp -> next;
+// }
+// return dummy -> next;
+//     }
+
+
+//optimal : O(N): keep connecting the exisitng
+ListNode* temp = new ListNode(-1);
+ListNode* dummy = temp;
 while(list1 && list2){
-    if(list1->val <= list2 -> val){
-        temp -> next = new ListNode(list1->val);
+    if(list1 -> val <= list2-> val){
+        temp -> next = list1;
         list1 = list1 -> next;
     }else{
-        temp -> next = new ListNode(list2 -> val);
+        temp -> next = list2;
         list2 = list2 -> next;
+
     }
     temp = temp -> next;
+
 }
-while(list1){
-    temp -> next = new ListNode(list1 -> val);
-    list1 = list1 -> next;
-    temp = temp -> next;
-}
-while(list2){
-    temp -> next = new ListNode(list2 -> val);
-    list2 = list2 -> next;
-    temp = temp -> next;
-}
+if(list1)temp->next = list1;
+else temp -> next = list2;
 return dummy -> next;
     }
 };
