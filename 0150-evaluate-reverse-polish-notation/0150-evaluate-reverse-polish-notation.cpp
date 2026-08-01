728e748b8f3c82bd3208eb702ffd66eb4659a222
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-01 19:54:46 +0530

    Time: 4 ms (19.2%), Space: 17 MB (88.24%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-        
-        for (string token : tokens) {
-            
-            if (token == "+" || token == "-" || token == "*" || token == "/") {
-                int b = st.top(); st.pop();
-                int a = st.top(); st.pop();
-                
-                if (token == "+"){
-st.push(a + b);
-                } 
-                else if (token == "-") {
-                    st.push(a - b);
-                }
-                else if (token == "*") {
-                    st.push(a * b);
-                }
-                else{
-st.push(a / b);  
-                } 
-            } 
-            else {
-                st.push(stoi(token));
-            }
-        }
-        
-        return st.top();
+  /*
+  Use a stack to store operands and evaluate whenever an operator appears*/      
+  /*
+  Number → push into stack
+Operator → pop 2 numbers, perform operation, push result back*/
+stack<int>st;
+for(string token : tokens){
+    if(token == "+" || token == "-" || token == "*" || token == "/"){ 
+        //agar operators haain
+        int num2 = st.top();
+        st.pop();
+        int num1 = st.top();
+        st.pop();
+if(token =="+") st.push(num1+num2);
+else if(token =="-")st.push(num1 - num2);
+else if(token =="*")st.push(num1 * num2);
+else st.push(num1 / num2);
+    }
+    else{
+        st.push(stoi(token));//number hai
+    }
+}
+return st.top();
     }
 };
