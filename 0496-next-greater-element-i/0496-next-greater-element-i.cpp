963063ac608068cc1a470b53b246a85e6519e913
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-01 15:15:48 +0530

    Time: 20 ms (5.87%), Space: 12.3 MB (86.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int , int>mpi;
-        stack<int>sti;
-        int n2 = nums2.size();
-        for(int i = n2 - 1; i >= 0 ; i--){
-            while(!sti.empty() && sti.top()<= nums2[i]){
-                sti.pop();
-            }
-            if(sti.empty()){
-            mpi[nums2[i]]= -1;
-            }
-            else{
-                mpi[nums2[i]] = sti.top();
-            }
-            sti.push(nums2[i]);
-        }
         int n1 = nums1.size();
-        vector<int>ans(n1);
-        for(int i = 0 ; i < n1 ; i++){
-            ans[i] = mpi[nums1[i]];
-        }
-        return ans;
+        int n2 = nums2.size();
+
+
+        //Brute Force: O(NXN) : search via nested loops
+        vector<int>indexes;
+for(int i = 0 ; i < n1 ; i++){
+for(int j = 0 ; j < n2 ; j++){
+if(nums1[i] == nums2[j]) indexes.push_back(j);
+}
+}
+vector<int>ans;
+for(int i = 0 ; i < indexes.size() ; i++){
+    bool possible = false;
+for(int j = indexes[i]+1 ; j < n2 ; j++){
+if(nums2[j] > nums2[indexes[i]]){
+    ans.push_back(nums2[j]);
+    possible = true;
+    break;
+}
+}
+if(possible == false) ans.push_back(-1);
+}
+return ans;
     }
 };
