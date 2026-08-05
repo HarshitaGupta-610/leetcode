663cae878b03e903dc90e93ef627e4bb49c81dc1
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-06 00:52:12 +0530

    Time: 546 ms (30.69%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,16 @@
-SELECT Weather.id
-FROM Weather
-JOIN Weather AS prev
-ON DATEDIFF(Weather.recordDate, prev.recordDate) = 1
-WHERE Weather.temperature > prev.temperature;
+# Write your MySQL query statement below
+-- We need to compare today's weather with yesterday's weather.
+-- Since both records are in the SAME table, we create TWO copies of the Weather table.
+-- w1 = Today's weather
+-- w2 = Yesterday's weather
+-- We need to compare today's weather with yesterday's weather.
+-- Since both records are in the SAME table, we create TWO copies of the Weather table.
+-- w1 = Today's weather
+-- w2 = Yesterday's weather
+
+SELECT w1.id
+FROM Weather w1
+-- Self Join: Joining the Weather table with itself
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
