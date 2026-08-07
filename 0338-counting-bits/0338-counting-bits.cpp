6244a2980f49390b4a476dab3d301b113a8516cd
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-07 16:16:36 +0530

    Time: 3 ms (23.25%), Space: 11.4 MB (8.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-      //Brute force: O( n logn)
+     
+      //Brute force: O( n logn): Har number ke liye check karo
+//       vector<int>ans;
+//     for(int num = 0 ; num <= n ; num++){//i ki value us number tak jayegi jo given hai
+//     int count = 0;
+//     int i = num;
+//    while(i != 0){
+//     int rem = i % 2;
+//     if(rem == 1) count++;
+//     i /= 2;
+//    }
+//    ans.push_back(count); 
+
+//     }  
+//     return ans;
+
+
+//Better Approach :O(n log n) using concept of last set bit and then right shifting by one
       vector<int>ans;
     for(int num = 0 ; num <= n ; num++){//i ki value us number tak jayegi jo given hai
     int count = 0;
     int i = num;
    while(i != 0){
-    int rem = i % 2;
-    if(rem == 1) count++;
-    i /= 2;
+    if(i & 1) count++;
+    i >>= 1;
    }
    ans.push_back(count); 
 
