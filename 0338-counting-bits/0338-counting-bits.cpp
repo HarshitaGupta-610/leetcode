db61ea3dd0de6da2aca51f25b6d01563c123b7f9
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-07 16:12:32 +0530

    Time: 4 ms (10.29%), Space: 11.4 MB (8.85%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>dp(n+1);
-        dp[0] = 0;
-        for(int i = 1 ; i <=n ; i++){
-            if(i % 2 == 0){
-            dp[i] = dp[i/2];
-            }
-            else{
-            dp[i] =dp[i / 2] +1;
-            }
-        }
-return dp;
+      //Brute force: O( n logn)
+      vector<int>ans;
+    for(int num = 0 ; num <= n ; num++){//i ki value us number tak jayegi jo given hai
+    int count = 0;
+    int i = num;
+   while(i != 0){
+    int rem = i % 2;
+    if(rem == 1) count++;
+    i /= 2;
+   }
+   ans.push_back(count); 
+
+    }  
+    return ans;
     }
 };
