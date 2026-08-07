1dd3799569d47c4d40e0f56ca01cb91c2663f153
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-07 13:11:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.35%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -11,11 +11,19 @@ public:
         // return count;
 
         //Better Force: O(log n) check last bit via and and then right shift
-        int count = 0;
-        while(n){
-if(n & 1) count++;
-n >>= 1;
-        }
-        return count;
+//         int count = 0;
+//         while(n){
+// if(n & 1) count++;
+// n >>= 1;
+//         }
+//         return count;
+
+//optimal : righmost set bit ko remove kro
+int count = 0;
+while(n){
+    n = n & (n-1);//We use n & (n - 1) because subtracting 1 from a number always changes its rightmost 1 bit to 0. When we perform AND with the original number, that rightmost 1 gets removed, while all the other higher bits remain the same.
+    count++;
+}
+return count;
     }
 };
