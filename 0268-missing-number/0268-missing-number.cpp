e177bcdae171b8b75adfd604e9075149ee0b56db
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-07 19:14:12 +0530

    Time: 6 ms (14.74%), Space: 27.9 MB (5.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,16 +4,29 @@ public:
         int n = nums.size();
 
         //Brute force: O(n²)
-        for(int i = 0 ; i <= n ; i++){
-            bool found = false;  
-            for(int j = 0 ; j < n ; j++){
-           if(nums[j] == i){
-            found = true;
-            break;
-           }
-            }
-            if(found == false) return i;
-        }
+        // for(int i = 0 ; i <= n ; i++){
+        //     bool found = false;  
+        //     for(int j = 0 ; j < n ; j++){
+        //    if(nums[j] == i){
+        //     found = true;
+        //     break;
+        //    }
+        //     }
+        //     if(found == false) return i;
+        // }
+
+//Brute force : O(N)
+//map mein daalo and see if frequency is 0 then it is the answer
+unordered_map<int,int>m;
+for(int num : nums){
+    m[num]++;
+}
+for(int i = 0 ; i <= n ; i++ ){
+    if(m[i] != 1){
+        return i;
+    }
+}
+
         
 return -1;
     }
