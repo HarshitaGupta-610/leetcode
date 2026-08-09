9d909784b6d769a58e556050fbc2a4521e6e2bd8
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-09 13:31:10 +0530

    Time: 1 ms (81.7%), Space: 14.9 MB (92.48%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
+         int n = nums.size();
+        
         //Brute Force : O(n² log n)
-        set<pair<int,int>>st;
-        int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i +1 ; j < n ; j++){
-                if(abs(nums[j] - nums[i])  ==  k){
-                    st.insert({
-                        min(nums[i] , nums[j]),
-                        max(nums[i] , nums[j])
-                    });
-                }
+        // set<pair<int,int>>st;
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = i +1 ; j < n ; j++){
+        //         if(abs(nums[j] - nums[i])  ==  k){
+        //             st.insert({
+        //                 min(nums[i] , nums[j]),
+        //                 max(nums[i] , nums[j])
+        //             });
+        //         }
+        //     }
+        // }
+        // return st.size();
+
+//Better:Sort the array and use two pointers to find pairs with difference k → O(n log n)
+    sort(nums.begin() , nums.end());
+        int i = 0 , j = 1, ans = 0;
+        while(j < n){
+            if(i == j) {
+                j++;
+                  continue;
+        }
+            int diff = nums[j] - nums[i];
+            if(diff == k){
+                ans++;
+            int left = nums[i];
+            int right = nums[j];
+            while( i < n && nums[i] == left)i++;
+            while( j < n && nums[j] == right)j++;
+            }else if(diff<k){
+                j++;
+            }else{
+                i++;
             }
         }
-        return st.size();
+        return ans;
     }
 };
