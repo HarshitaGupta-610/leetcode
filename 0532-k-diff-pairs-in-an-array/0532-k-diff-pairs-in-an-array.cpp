027adad98a9adec55816abc6403aea4e670fdfeb
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-09 13:41:43 +0530

    Time: 11 ms (38.83%), Space: 17.1 MB (40.85%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -18,24 +18,46 @@ public:
         // return st.size();
 
 //Better:Sort the array and use two pointers to find pairs with difference k → O(n log n)
-    sort(nums.begin() , nums.end());
-        int i = 0 , j = 1, ans = 0;
-        while(j < n){
-            if(i == j) {
-                j++;
-                  continue;
-        }
-            int diff = nums[j] - nums[i];
-            if(diff == k){
-                ans++;
-            int left = nums[i];
-            int right = nums[j];
-            while( i < n && nums[i] == left)i++;
-            while( j < n && nums[j] == right)j++;
-            }else if(diff<k){
-                j++;
+    // sort(nums.begin() , nums.end());
+    //     int i = 0 , j = 1, ans = 0;
+    //     while(j < n){
+    //         if(i == j) {
+    //             j++;
+    //               continue;
+    //     }
+    //         int diff = nums[j] - nums[i];
+    //         if(diff == k){
+    //             ans++;
+    //         int left = nums[i];
+    //         int right = nums[j];
+    //         while( i < n && nums[i] == left)i++;
+    //         while( j < n && nums[j] == right)j++;
+    //         }else if(diff<k){
+    //             j++;
+    //         }else{
+    //             i++;
+    //         }
+    //     }
+    //     return ans;
+
+
+
+//Optimal:unordered_map me har number ki frequency store karo, phir har unique x ke liye check karo ki x + k exist karta hai ya nahi; k=0 me frequency >=2 honi chahiye → O(n) , x+k and x-k mein se ek hi! Dono valid hain, but same pair opposite direction se 2 baar count ho jayega, isliye unique pairs ke liye ek hi direction (x+k) check karte hain.
+        unordered_map<int,int>m;
+        for(int num : nums)
+        {
+          m[num]++;  
+        }   
+        int ans = 0;
+        for(auto &it : m){
+            if(k == 0){
+                if(it.second >=2){
+                    ans++;
+                }
             }else{
-                i++;
+                if(m.count(it.first+k)){
+                    ans++;
+                }
             }
         }
         return ans;
