c07495cbe8045486256d6fdaab342f8861ef768b
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-15 12:44:17 +0530

    Time: 0 ms (100%), Space: 10.5 MB (94.5%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+
+        //Brute: both time and space O(N):Traverse the linked list and store all binary digits in a vector.Reverse the vector.Multiply each digit by its corresponding power of 2.Add everything to ans.
+        vector<int>bdigits;
+        while(head){
+            bdigits.push_back(head ->val);
+            head = head -> next;
+        }
+        reverse(bdigits.begin() , bdigits.end());
+        int ans = 0;
+        int n = bdigits.size();
+        int pow =1;
+        for(int i = 0 ; i< n ; i++){
+            ans = ans + (pow*bdigits[i]);
+            pow*=2;
+        }
+        return ans;
+
+    }
+};
