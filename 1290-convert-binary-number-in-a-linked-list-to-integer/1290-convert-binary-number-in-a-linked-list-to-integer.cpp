6ba1965c97db876f2035de9442735eaf6e45571f
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-15 12:58:58 +0530

    Time: 0 ms (100%), Space: 10.6 MB (41.37%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -13,20 +13,27 @@ public:
     int getDecimalValue(ListNode* head) {
 
         //Brute: both time and space O(N):Traverse the linked list and store all binary digits in a vector.Reverse the vector.Multiply each digit by its corresponding power of 2.Add everything to ans.
-        vector<int>bdigits;
-        while(head){
-            bdigits.push_back(head ->val);
-            head = head -> next;
-        }
-        reverse(bdigits.begin() , bdigits.end());
-        int ans = 0;
-        int n = bdigits.size();
-        int pow =1;
-        for(int i = 0 ; i< n ; i++){
-            ans = ans + (pow*bdigits[i]);
-            pow*=2;
-        }
-        return ans;
+        // vector<int>bdigits;
+        // while(head){
+        //     bdigits.push_back(head ->val);
+        //     head = head -> next;
+        // }
+        // reverse(bdigits.begin() , bdigits.end());
+        // int ans = 0;
+        // int n = bdigits.size();
+        // int pow =1;
+        // for(int i = 0 ; i< n ; i++){
+        //     ans = ans + (pow*bdigits[i]);
+        //     pow*=2;
+        // }
+        // return ans;
 
+//Optimal Appraoch :Har new binary digit ke liye: old answer ko 2 se multiply karo, phir current bit add karo. TC : O(N)
+int ans = 0;
+while(head){
+    ans = ans * 2 + head->val;
+    head = head->next;
+}
+return ans;
     }
 };
