205d92373cc46e482cda3d376857938bfca8c153
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-19 00:30:28 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!= t.size()){
+
+
+
+//   //O(nlogn): sort and check
+//    if (s.size() != t.size())
+//             return false;
+
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+
+//         return s == t;     
+//     }
+
+
+//Tc:O(n)
+if (s.size() != t.size())
             return false;
+
+        int freq[26] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
-        unordered_map<char , int>m;
-        for(char c: s){
-            m[c]++;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
         }
-       for(char c : t){
-        if(m.find(c) == m.end()){
-            return false;
-        }
-        else{
-            m[c]--;
-        }
-        if(m[c] < 0){
-        return false;
-        }
-       } 
-       return true;
+
+        return true;
     }
 };
