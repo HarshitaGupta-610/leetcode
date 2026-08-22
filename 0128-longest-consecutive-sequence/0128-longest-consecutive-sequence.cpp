c23fbdc15999ccf4bcedca4d4670109adfbce25c
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-08-22 16:24:04 +0530

    Time: 87 ms (31.07%), Space: 89 MB (25.41%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -18,16 +18,32 @@ public:
 
 
 //Better : O(nlogn)
-sort(nums.begin() , nums.end());
-if(nums.empty()) return {};
-int ans =1;
-int curr = 1;
-for(int i = 1 ; i < nums.size() ; i++){
-    if(nums[i] == nums[i-1]) continue;
-    if(nums[i] == nums[i-1] +1)curr++;
-    else curr = 1;
-    ans = max(ans,curr);
+// sort(nums.begin() , nums.end());
+// if(nums.empty()) return {};
+// int ans =1;
+// int curr = 1;
+// for(int i = 1 ; i < nums.size() ; i++){
+//     if(nums[i] == nums[i-1]) continue;
+//     if(nums[i] == nums[i-1] +1)curr++;
+//     else curr = 1;
+//     ans = max(ans,curr);
+// }
+// return ans;
+
+
+//Optimal : O(N) use of set
+unordered_set<int>s(nums.begin() , nums.end());
+int longest = 0;
+for(int num : s){
+if(s.find(num-1) != s.end()) continue;
+int curr = num;
+int count =1;
+while(s.find(curr +1) != s.end()){
+    curr++;
+    count++;
 }
-return ans;
+longest = max(longest , count);
+}
+return longest;
     }
 };
