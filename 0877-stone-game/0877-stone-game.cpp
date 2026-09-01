393b6c0b8fa024f0fa071e6cc20a3527c768813b
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-09-01 20:35:17 +0530

    Time: 0 ms (100%), Space: 10.6 MB (35.62%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-    //alice bob alice bob , size hi even hai humesha alice jeetegei
-    return true;  
+   return true;     
     }
 };
