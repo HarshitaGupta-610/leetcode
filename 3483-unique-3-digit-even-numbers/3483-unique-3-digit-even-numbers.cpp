1ed4e3a9cbf27b94dde2870467a6d064c35764df
Harshita Gupta <harshitagupta.ambala@gmail.com>
2026-09-11 16:01:12 +0530

    Time: 0 ms (100%), Space: 28.8 MB (88.78%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+// freq[i] stores how many times digit iis present in the given array
+int freq[10] = {0};
+for (int d : digits) freq[d]++;
+int ans = 0;
+for (int last = 0; last <= 8; last += 2) {
+if (freq[last] == 0)continue;
+freq[last]--;
+for (int first = 1; first <= 9; first++) {
+if (freq[first] == 0)continue;
+freq[first]--;  
+for (int middle = 0; middle <= 9; middle++) {
+if (freq[middle] > 0) ans++;                    
+}
+freq[first]++;
+}
+freq[last]++;
+}
+return ans;
+    }
+};
